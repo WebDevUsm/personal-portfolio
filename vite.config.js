42964e46f01696cb359400678df932314168feae
UsmanShahid <muhammadusmanshahid789@gmail.com>
2024-08-28 06:10:46 +0500

    increase limit size

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Increase the chunk size warning limit (e.g., to 1000 kB)
+    chunkSizeWarningLimit: 1000,
+
+    // Custom manual chunking to split vendor code
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 })
